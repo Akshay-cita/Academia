2e383ff2052ddf30d0ea3e1e9ebbd4f5b1bff163
Akshay-cita <edakkatakshay@gmail.com>
2020-12-25 23:52:39 +0530

    modified settings

--- Academia/settings.py
+++ Academia/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'usersApp',
     'academics',
-    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
