a6bf0dd8ea564d1d80c157242c78e445c625c1ad
Akshay-cita <edakkatakshay@gmail.com>
2020-12-27 01:14:56 +0530

    relations added academics model

--- academics/models.py
+++ academics/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class Department(models.Model):
 	dept_name=models.CharField(max_length=50)
-	# hod_id=models.ForeignKey(Hod,on_delete=models.CASCADE())
-	# staff_id=models.ForeignKey(Staff,on_delete=models.CASCADE())
+	hod_id=models.ForeignKey(Hod,on_delete=models.CASCADE())
+	
 
 
 	def __str__(self):
@@ -13,8 +13,7 @@ class Department(models.Model):
 
 class Course(models.Model):
 	course_name=models.CharField(max_length=100)
-	# course_code=models.CharField(max_length=100)
-	# dept_id=models.ForeignKey(Department,on_delete=models.CASCADE())
+	dept_id=models.ForeignKey(Department,on_delete=models.CASCADE())
 
 	def __str__(self):
 		return self.course_name
@@ -22,8 +21,9 @@ class Course(models.Model):
 class Subject(models.Model):
 	subject_name=models.CharField(max_length=100)
 	subject_code=models.CharField(max_length=100)
-	# dept_id=models.ForeignKey(Department,on_delete=models.CASCADE())
-	# staff_id=models.ForeignKey(Staff,on_delete=models.CASCADE())
+	course_id=models.ForeignKey(Course,on_delete=models.CASCADE())
+	dept_id=models.ForeignKey(Department,on_delete=models.CASCADE())
+	
 
 
 
