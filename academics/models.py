f34f9ca73f6c8acb90520db3fbfd494d9bfa577b
Akshay-cita <edakkatakshay@gmail.com>
2020-12-27 01:08:28 +0530

    Department,subject,course added

--- academics/models.py
+++ academics/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+# from usersApp.models import Hod,Staff
 
 # Create your models here.
+class Department(models.Model):
+	dept_name=models.CharField(max_length=50)
+	# hod_id=models.ForeignKey(Hod,on_delete=models.CASCADE())
+	# staff_id=models.ForeignKey(Staff,on_delete=models.CASCADE())
+
+
+	def __str__(self):
+		return self.dept_name
+
+class Course(models.Model):
+	course_name=models.CharField(max_length=100)
+	# course_code=models.CharField(max_length=100)
+	# dept_id=models.ForeignKey(Department,on_delete=models.CASCADE())
+
+	def __str__(self):
+		return self.course_name
+
+class Subject(models.Model):
+	subject_name=models.CharField(max_length=100)
+	subject_code=models.CharField(max_length=100)
+	# dept_id=models.ForeignKey(Department,on_delete=models.CASCADE())
+	# staff_id=models.ForeignKey(Staff,on_delete=models.CASCADE())
+
+
+
+	def __str__(self):
+		return self.subject_name
+
